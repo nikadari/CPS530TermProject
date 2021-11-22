88082c1c0b90a6bf628ac7a9c03931237114a071
ayalcin99 <ayalcin@ryerson.ca>
2021-11-22 14:45:39 -0500

    Started working on the app.js and created posts.js to deal with posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Topbar from './components/topbar/Topbar';
+import React from 'react';
+import Posts from '/.Posts';
+
 //import './App.css';
 
 function App() {
   return (
-    <Topbar />
-    // <div className="App">
-    //   <header className="App-header">
-        
-        
-    //   </header>
-    // </div>
+    <Topbar>
+    <div className="App">
+
+      <div className = "app__header"> 
+        <img className = "app__headerImage"
+          alt="">
+          {/* add header logo here */}
+    
+    
+        </img>
+    
+      </div>
+      <h1>Hello to your page of MyLife</h1>
+
+      <Posts />
+      {/*Header */}
+      {/*Posts */}
+
+      {/*Post */}
+       
+    </div>
+
+    
+
+
+
+    </Topbar>
   );
 }
 
