6a431fc92fe87d0b97f8cbb3e4756191b6a71475
ayalcin99 <ayalcin@ryerson.ca>
2021-12-02 12:55:10 -0500

    Finished setting up post and avatar pictures

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import Topbar from './components/topbar/Topbar';
 import React from 'react';
 import Posts from './Posts';
 import './App.css';
+import dog from "./resources/dog.jpg"
+import bunny from "./resources/bunny.jpg"
+import coffee from "./resources/coffee picture.jpg"
+import avatar1 from "./resources/avar1.jpg"
+import avatar2 from "./resources/avatar2.jpg"
+import avatar3 from "./resources/avatar3.png"
 
 function App() {
   return (
     <>
       <Topbar /> 
       <h1>Welcome to MyLife</h1>
-      <Posts username="ayalcin" caption="So cute"/>
-      <Posts username="irmakyalcn" caption="So happy"  />
-      <Posts username="brap" caption= "you rock puppy"/>
+      <Posts username="ayalcin" caption="So cute" imgLocation = {dog} avatar = {avatar1}/>
+      <Posts username="irmakyalcn" caption="Bunny in a basket" imgLocation = {bunny} avatar = {avatar2} />
+      <Posts username="brap" caption= "Made myself nice latte with art" imgLocation = {coffee} avatar = {avatar3}/>
 
    </>
 
--- client/src/Posts.js
+++ client/src/Posts.js
@@ -4,18 +4,19 @@ import Profile from "@material-ui/core/Avatar";
 
 
 
-function Posts({username, caption}) {
+function Posts({username, caption, imgLocation, avatar}) {
     return (
         <div className="posts">
             <div className = "profile_header">
                 <Profile
                     className="profile_pic"
                     alt='Person'
+                    src = {avatar}
             
                 />
                 <h3>{username}</h3>
             </div>
-            <img className="post_image" src = {require('./resources/dog.jpg')}
+            <img className="post_image" src = {imgLocation}
                 alt=''></img>
 
             {/*header -> profile pic and username */}
