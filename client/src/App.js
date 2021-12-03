0e041626aed8469ed7a2c23046dc991755cc93db
ayalcin99 <ayalcin@ryerson.ca>
2021-12-03 17:09:09 -0500

    Finished adding more comments

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,9 @@ function App() {
     <>
       <Topbar /> 
       <h1>Welcome to MyLife</h1>
-      <Posts username="ayalcin" caption="So cute" imgLocation = {dog} avatar = {avatar1}/>
-      <Posts username="irmakyalcn" caption="Bunny in a basket" imgLocation = {bunny} avatar = {avatar2} />
-      <Posts username="brap" caption= "Made myself nice latte with art" imgLocation = {coffee} avatar = {avatar3}/>
+      <Posts username="ayalcin" caption="My cute Bailey" imgLocation = {dog} avatar = {avatar1} username2 = "blip" comment = "So cute" username3 = "michael78" comment1 = "OMG I want a dog rn"/>
+      <Posts username="irmakyalcn" caption="Bunny in a basket" imgLocation = {bunny} avatar = {avatar2} username2 = "may123" comment = "I wish I had a bunny like that so cute" username3 = "jennyClarke" comment1 = "OMG"/>
+      <Posts username="brap" caption= "Made myself nice latte with art" imgLocation = {coffee} avatar = {avatar3} username2 = "taylor123" comment = "So jealous" username3 = "ryanHoward" comment1 = "Can you make some latte too? pls"/>
 
    </>
 
--- client/src/Posts.js
+++ client/src/Posts.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import './Posts.css';
 import Profile from "@material-ui/core/Avatar";
-import { useState } from "react";
-import likeImg from "../../resources/like.png";
 
 
 
 function Posts({username, caption, imgLocation, avatar, username2, comment, username3, comment1}) {
-    /*state variables needed for likes */
-    const [likes, setLikes] = useState(0)
-    const [isLiked, setIsLiked] = useState(false)
-
-    /* set state function, determines if like button is pressed or not */
-    const likeEvent =() =>{
-        setLikes(isLiked ? likes-1 : likes+1)
-        setIsLiked(!isLiked)
-    }
-        
+     {/*state variables needed for likes */}
+     const [likes, setLikes] = useState(0)
+     const [isLiked, setIsLiked] = useState(false)
+ 
+     {/* set state function, determines if like button is pressed or not */}
+     const likeEvent =() =>{
+         setLikes(isLiked ? likes-1 : likes+1)
+         setIsLiked(!isLiked)
+     }
     return (
-
         <div className="posts">
-            {/*header -> profile pic and username */}
             <div className = "profile_header">
                 <Profile
                     className="profile_pic"
@@ -30,20 +25,21 @@ function Posts({username, caption, imgLocation, avatar, username2, comment, user
                 />
                 <h3>{username}</h3>
             </div>
-            {/*image */}    
             <img className="post_image" src = {imgLocation}
                 alt=''></img>
 
+            {/*header -> profile pic and username */}
+            {/* like button */} 
             <div className="CaptionLike">
                 <img className="likeBtn" src={likeImg} onClick={likeEvent}/>
                 <h4 className="Caption"><strong>{likes} likes </strong></h4>
             </div>
 
-            {/*username + caption*/}
+            {/*image */}
             <h4 className="Caption"><strong>{username}</strong> <weak>{caption}</weak></h4>
             <h5 className="Caption"><strong>{username2}</strong> <weak>{comment}</weak></h5>
             <h6 className="Caption"><strong>{username3}</strong> <weak>{comment1}</weak></h6>
-            
+            {/*username + caption*/}
             
             
         </div>
